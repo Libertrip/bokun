036f249d741675905866f735aac280773f4cfda0
MaximeRDY <maxime.raverdy@gmail.com>
2016-09-08 16:56:35 +0200

    Can Book and Cancel a Product

--- lib/bokun/booking.ex
+++ lib/bokun/booking.ex
@@ -2,4 +2,34 @@ defmodule Bokun.Booking do
   @moduledoc """
     An module for All Booking
   """
+
+  @doc """
+    Cancel booking
+  ## Example
+      Bokun.Booking.cancel_booking("ICT-8582", %{
+        "note": "We want to cancel.",
+        "refund": true,
+        "notify": true
+      })
+  """
+  def cancel_booking(booking_confirmation_code, body, params \\ %{}) do
+    Bokun.post_request("/booking.json/cancel-booking/#{booking_confirmation_code}", body, set_default_currency_and_lang(params))
+  end
+
+  @doc """
+    Cancel Product Booking
+  ## Example
+      Bokun.Booking.cancel_booking("ICT-T5312", %{
+        "note": "We want to cancel.",
+        "refund": true,
+        "notify": true
+      })
+  """
+  def cancel_product_booking(production_confirmation_code, body, params \\ %{}) do
+    Bokun.post_request("/booking.json/cancel-product-booking/#{production_confirmation_code}", body, set_default_currency_and_lang(params))
+  end
+
+  defp set_default_currency_and_lang(params) do
+    Map.merge %{currency: "EUR", lang: "FR"}, params
+  end
 end
