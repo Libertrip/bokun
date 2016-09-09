8a826f02390cd2524442263a8277b3d0b100d177
MaximeRDY <maxime.raverdy@gmail.com>
2016-09-09 09:56:29 +0200

    Change Httpoison to tesla for log some request in elasticSearch in future

--- lib/bokun.ex
+++ lib/bokun.ex
@@ -6,36 +6,24 @@ defmodule Bokun do
 
   # Let's build on top of HTTPoison
   use Application
-  use HTTPoison.Base
-  require IEx
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl,  Application.get_env(:bokun, :api_url) || "http://api.bokundemo.com"
+  plug Tesla.Middleware.JSON
+  plug Tesla.Middleware.DebugLogger
 
   def start(_type, _args) do
     Bokun.Supervisor.start_link
   end
 
   def post_request(endpoint, body \\ %{}, params \\ %{}) do
-    Bokun.post!(endpoint, Poison.encode!(body), parse_headers("POST", endpoint, params), [params: params])
+    headers = parse_headers("POST", endpoint, params)
+    Bokun.post(endpoint, body,  query: params, headers: headers)
   end
 
   def get_request(endpoint, params \\ %{}) do
-    header = parse_headers("GET", endpoint, params)
-    Bokun.get!(endpoint, header, [params: params])
-  end
-
-  @doc """
-    Creates the URL for our endpoint
-    Args:
-      * endpoint - part of the API we're hitting
-    Returns string
-  """
-  def process_url(endpoint) do
-    base_url() <> endpoint
-  end
-
-  def process_response_body(body) do
-    Poison.decode!(body)
-  rescue
-    _ -> body
+    headers = parse_headers("GET", endpoint, params)
+    Bokun.get(endpoint, query: params, headers: headers)
   end
 
   def access_key do
@@ -46,24 +34,15 @@ defmodule Bokun do
     Application.get_env(:bokun, :secret_key)
   end
 
-
-  defp base_url do
-    if Application.get_env(:bokun, :api_url) do
-      Application.get_env(:bokun, :api_url)
-    else
-      "http://api.bokundemo.com"
-    end
-  end
-
   defp parse_headers(method, endpoint, params) do
     current_time = Timex.format!(Timex.now, "%F %T", :strftime)
-    [
+    %{
       "Content-Type": "application/json",
       "Accept": "application/json",
       "X-Bokun-AccessKey": access_key,
       "X-Bokun-Date": current_time,
       "X-Bokun-Signature": get_signature(method, endpoint, params, current_time)
-    ]
+    }
   end
 
   defp get_signature(method, endpoint, params, current_time) do
